f8bac9a00713614b971821e8d12b526aa90b9d97
Ali Newaz <ali.newaz@periscopelabs.io>
2023-07-04 13:50:49 +0600

    reset table mapping

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -12,11 +12,11 @@ function onDragStart(event, nodeType) {
     <div class="description">You can drag these nodes to the pane.</div>
 
     <div class="nodes">
-      <div class="vue-flow__node-input" :draggable="true" @dragstart="onDragStart($event, 'input')">Input Node</div>
+      <div class="vue-flow__node-input" :draggable="true" @dragstart="onDragStart($event, 'default')">Table One</div>
 
-      <div class="vue-flow__node-default" :draggable="true" @dragstart="onDragStart($event, 'default')">Default Node</div>
+      <div class="vue-flow__node-default" :draggable="true" @dragstart="onDragStart($event, 'default')">Table Two</div>
 
-      <div class="vue-flow__node-output" :draggable="true" @dragstart="onDragStart($event, 'output')">Output Node</div>
+      <div class="vue-flow__node-output" :draggable="true" @dragstart="onDragStart($event, 'default')">Table Three</div>
     </div>
   </aside>
 </template>
--- src/components/TableMap.vue
+++ src/components/TableMap.vue
@@ -1,14 +1,16 @@
 <script setup>
-import { VueFlow, useVueFlow } from '@vue-flow/core'
+import { Panel, PanelPosition, VueFlow, useVueFlow } from '@vue-flow/core'
 import { nextTick, watch } from 'vue'
 import Sidebar from './Sidebar.vue'
+import { ref } from 'vue'
 
+let tables = ref([])
 let id = 0
 function getId() {
   return `dndnode_${id++}`
 }
 
-const { findNode, onConnect, addEdges, addNodes, project, vueFlowRef } = useVueFlow({
+const { findNode, onConnect, addEdges, addNodes, project, vueFlowRef, $reset } = useVueFlow({
   nodes: [
     {
       id: '1',
@@ -19,6 +21,10 @@ const { findNode, onConnect, addEdges, addNodes, project, vueFlowRef } = useVueF
   ],
 })
 
+function resetTransform() {
+  tables = [];
+  $reset()
+}
 function onDragOver(event) {
   event.preventDefault()
 
@@ -61,6 +67,7 @@ function onDrop(event) {
       },
       { deep: true, flush: 'post' },
     )
+    console.log(node);
   })
 }
 </script>
@@ -68,7 +75,15 @@ function onDrop(event) {
 <template>
   <div class="dndflow" @drop="onDrop">
     <Sidebar />
-    <VueFlow @dragover="onDragOver" />
+    <VueFlow v-model="tables" @dragover="onDragOver" >
+        <Panel :position="PanelPosition.TopRight" class="controls">
+            <button style="background-color: #113285; color: white" title="Reset Transform" @click="resetTransform">
+                <svg width="16" height="16" viewBox="0 0 32 32">
+                <path fill="#FFFFFB" d="M18 28A12 12 0 1 0 6 16v6.2l-3.6-3.6L1 20l6 6l6-6l-1.4-1.4L8 22.2V16a10 10 0 1 1 10 10Z" />
+                </svg>
+            </button>
+        </Panel>
+    </VueFlow>
   </div>
 </template>
 <style scoped>
