81701c2eb7c1e22f1a89818891e0d1cef4bb58a4
SkMdJoynulAbedin <joynul.abedin@periscopelabs.io>
2023-07-05 13:44:07 +0600

    table done

---
+++ src/components/ReportTable.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container my-4">
+    <div class="d-flex justify-content-between align-items-center mt-2">
+      <router-link :to="{ name: 'table-map' }" class="text-decoration-none">
+        <i class="fa-solid fa-arrow-left me-2"></i>Table Map
+      </router-link>
+      <div class="dropdown">
+        <button class="btn btn-outline-primary btn-sm fw-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Export
+        </button>
+        <div class="dropdown-menu">
+          <button class="dropdown-item fw-light" type="button">PDF</button>
+          <button class="dropdown-item fw-light" type="button">EXCEL</button>
+          <button class="dropdown-item fw-light" type="button">CSV</button>
+        </div>
+      </div>
+    </div>
+    <div class="mt-3">
+      <h5 class="fw-normal">Report Name</h5>
+      <p class=" fw-normal">Report Description</p>
+    </div>
+    <hr style="margin-top: 30px;">
+    <div>
+      <div class="card mt-5 py-2">
+        <pre class="pre-scrollable border-start border-primary border-5 rounded" data-spy="scroll" style="height: 200px; margin-bottom: 0px;">
+          SELECT 
+            translation, 
+            language_id, 
+            name 
+          FROM 
+            vw_translate_all 
+          where 
+            language_id = 1 
+            and type = 'label' 
+          group by 
+            translation
+        </pre>
+      </div>
+    </div>
+    <div class="table-responsive mt-5">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+  
+<script>
+export default {
+  components:{
+  },
+  data() {
+    return {
+    }
+  },
+  created() {
+  },
+  mounted() {
+  },
+  watch: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/TableMap.vue
+++ src/components/TableMap.vue
@@ -79,11 +79,16 @@ function onDrop(event) {
     <div class="d-flex flex-column justify-content-center align-items-center w-100" style="height: 100vh;">
       <VueFlow v-model="tables" @dragover="onDragOver" >
         <Panel :position="PanelPosition.TopRight" class="controls">
-            <button style="background-color: #113285; color: white" title="Reset Transform" @click="resetTransform">
-                <svg width="16" height="16" viewBox="0 0 32 32">
-                <path fill="#FFFFFB" d="M18 28A12 12 0 1 0 6 16v6.2l-3.6-3.6L1 20l6 6l6-6l-1.4-1.4L8 22.2V16a10 10 0 1 1 10 10Z" />
-                </svg>
-            </button>
+            <div class="d-flex justify-content-center align-items-center">
+              <button @click="$router.push({ name: 'preview'})" type="button" class="btn btn-outline-primary btn-sm me-2">
+                Preview
+              </button>
+              <button style="background-color: #113285; color: white" title="Reset Transform" @click="resetTransform">
+                  <svg width="16" height="16" viewBox="0 0 32 32">
+                  <path fill="#FFFFFB" d="M18 28A12 12 0 1 0 6 16v6.2l-3.6-3.6L1 20l6 6l6-6l-1.4-1.4L8 22.2V16a10 10 0 1 1 10 10Z" />
+                  </svg>
+              </button>
+            </div>
         </Panel>
       </VueFlow>
       <OptionsPen />
